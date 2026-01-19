329aba6f145155c2316950a80dd647f82fda71d5
Atelier Ferrohenge <kai@ferrohenge.com>
2026-01-19 16:31:10 +0100

    Add interaction control virtual function

--- src/element.cpp
+++ src/element.cpp
@@ -8,9 +8,18 @@ StringName Element::get_name() const {
     return name;
 }
 
+Variant Element::get_interaction(StringName p_element) const {
+    Variant ret;
+    GDVIRTUAL_CALL(_get_interaction, p_element, ret);
+    return ret;
+}
+
 void Element::_bind_methods() {
     ClassDB::bind_method(D_METHOD("set_name", "name"), &Element::set_name);
     ClassDB::bind_method(D_METHOD("get_name"), &Element::get_name);
+
+    // Standardize either using semicolons here or not
+    GDVIRTUAL_BIND(_get_interaction);
 
     ADD_PROPERTY(PropertyInfo(Variant::STRING_NAME, "name"), "set_name", "get_name");
 }
--- src/element.h
+++ src/element.h
@@ -2,6 +2,8 @@
 
 #include "godot_cpp/classes/resource.hpp"
 
+#include <godot_cpp/core/gdvirtual.gen.inc>
+
 using namespace godot;
 
 class Element : public Resource {
@@ -14,8 +16,12 @@ public:
     void set_name(StringName p_name);
     StringName get_name() const;
 
+    virtual Variant get_interaction(StringName element) const;
+
 protected:
     static void _bind_methods();
+
+    GDVIRTUAL1RC_REQUIRED(Variant, _get_interaction, StringName)
 
 private:
     StringName name = "";
